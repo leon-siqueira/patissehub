caa2e67df67fbd31097a7ac032e034b9c31fd031
leon-siqueira <leon.siqueir4@gmail.com>
2022-03-02 14:35:17 -0300

    devise installed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+  # devise configuration
+  config.action_mailer.default_url_options = { host: '$HEROKU_HOST_HERE', port: 3000 }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
