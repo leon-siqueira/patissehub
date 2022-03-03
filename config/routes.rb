4aa11c8a68483fae9a64f2d223c166e763dd0e6a
Maressa Alves <maressa.msoa@gmail.com>
2022-03-03 17:59:32 -0300

    add my_profile e profile na view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, only: %i[index]
   root to: 'pages#home'
-  get "/profile", to: "pages#profile"
+  get "/my_profile", to: "pages#my_profile", as: "my_profile"
+  get "/profile/:id", to: "pages#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
