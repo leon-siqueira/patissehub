d9771c2f3e8c59b2b35dd1c062a4ccbdf6fed382
leon-siqueira <leon.siqueir4@gmail.com>
2022-03-04 16:07:02 -0300

    new index visual

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def pluralize(word, number)
-    number > 1 ? "#{number} #{word}s" : "#{number} #{word}"
+    number != 1 ? "#{number} #{word}s" : "#{number} #{word}"
   end
 end
