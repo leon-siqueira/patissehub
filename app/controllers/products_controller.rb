3254dfdacd9e41033303a7ddaf62db0fcb27a479
c <caio.asevedo@gmail.com>
2022-03-07 16:35:02 -0300

    added new search and results

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,14 +2,11 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    # if params[:query].present?
-    #   @query = params[:query]
-    #   @products = Product.where(params[:query])
-    #   # Preventing SQL Injection and Database error for
-    #   # unknown characters
-    # else
-    @products = Product.all
-    # end
+    if params[:query]
+      @products = Product.search_by_name_and_description(params[:query])
+    else
+      @products = Product.all
+    end
   end
 
   def show
