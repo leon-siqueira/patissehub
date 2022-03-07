74a0c2ee8ab294c8833ddb6e81b68e95ae5d5ed8
Maressa Alves <maressa.msoa@gmail.com>
2022-03-07 15:56:36 -0300

    adicionando rotas que faltaram no ultimo commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,30 +12,17 @@ class ProductsController < ApplicationController
     # end
   end
 
-  def create
-    @product = Product.new(product_params)
-    @product.user_id = current_user.id
-    @product.save
-
-    redirect_to products_path(@product)
-  end
-
-  def new
-    @product = Product.new
-  end
-
   def show
-    @product = Product.find(id: params[:id])
   end
 
   def edit
-    
+    @product = Product.find(params[:id])
   end
 
   def update
     @product.update(product_params)
 
-    redirect_to product_path(@product)
+    redirect_to my_profile_path
   end
 
   def destroy
